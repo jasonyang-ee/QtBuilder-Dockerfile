55917270d121d2d9dfaacf301c6f5ef99afc4c78
Jason Teng <git@jasony.org>
2023-12-07 20:00:24 -0800

    Edit: clean up comment section and rename files

---
+++ Build-QtDev.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+version=6.6.0
+
+# Build Qt Developer Image
+docker buildx build --platform linux/amd64,linux/arm64 --build-arg QTVER=$version -t qt-dev:6 -f ./QtDev/Dockerfile.dev ./QtDev
