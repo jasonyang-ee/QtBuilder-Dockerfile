aa0f1175419051d797c016791f9d1c5a6e4726c9
Jason Yang <git@jasony.org>
2024-02-09 14:00:10 -0800

    Edit: using buildx to output file and using zip source

--- Build-QtBuilder.sh
+++ Build-QtBuilder.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-
 # Build Qt Builder Image
-docker buildx build --platform linux/amd64 -t qt-builder:6 -f ./QtBuilder/Dockerfile.builder ./QtBuilder
+docker buildx build --target=artifact --output type=local,dest=$(pwd)/build/ --platform linux/amd64,linux/arm64 -t qt-builder:latest -f ./QtBuilder/Dockerfile.builder ./QtBuilder
+
+if [ -x "$(command -v ntfy)" ]; then ntfy send "build qt-builder complete"; fi
